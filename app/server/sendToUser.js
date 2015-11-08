f5198444e2e2c3d2c94e9a5a29f4936dacc88365
ben <winckell.benjamin.isart@gmail.com>
2015-11-08 19:42:08 +0100

    add new tool for send back data or log to front user

---
+++ app/server/sendToUser.js
@@ -0,0 +1,21 @@
+function sendToUser (status,message,dataObj){
+    var obj;
+    if((status===undefined|| status===null )&&(status !=='error' ||status !=='success'))
+        return "Problem Status="+status + ". type of status value allowed :error and success ";
+    if((message===undefined|| message===null )&&message.length <=0 && typeof message !=='string')
+        return "Problem Message="+message;
+
+    obj = {
+        status : status,
+        message:message
+    };
+
+    if((dataObj!==undefined|| dataObj!==null ) && typeof dataObj === 'object')
+        obj.data = JSON.stringify(dataObj);
+    else
+        obj.data = {};
+
+    return obj;
+}
+
+module.exports = sendToUser;
