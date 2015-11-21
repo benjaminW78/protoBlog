1cc93bd4d5f33bb190a3ba17685bf1ce648500a9
ben <winckell.benjamin.isart@gmail.com>
2015-11-21 16:16:28 +0100

    add new comment inside this file

---
+++ app/vendors/appAngular/services/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+var app = require('angular').module('Blog');
+ // app.controller('connectionCtrl', require('./connection.ctrl.js'));
+// app.controller('FooterCtrl', require('./footer'));
+// app.controller('TodoCtrl', require('./todo'));
+// app.controller('TodoListCtrl', require('./todo_list'));
+// app.controller('ImprintCtrl', require('./imprint'));
+module.exports=this;
