4677311de0332d53811e1b23140a6a72a0608cdb
ben <winckell.benjamin.isart@gmail.com>
2016-03-20 05:55:51 +0100

    fix 04

--- app/angularApps/front/controllers/index.js
+++ app/angularApps/front/controllers/index.js
@@ -5,6 +5,4 @@ app.controller('homeCtrl', require('./home.ctrl.js'));
 app.controller('connectionCtrl', require('./connection.ctrl.js'));
 // app.controller('FooterCtrl', require('./footer'));
 // app.controller('TodoCtrl', require('./todo'));
-// app.controller('TodoListCtrl', require('./todo_list'));
-// app.controller('ImprintCtrl', require('./imprint'));
 
