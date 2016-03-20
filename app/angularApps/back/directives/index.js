9771b6365d1f98593777ac03fa46321e0b10bbfc
ben <winckell.benjamin.isart@gmail.com>
2016-03-20 21:52:35 +0100

    add new directive for list images

--- app/angularApps/back/directives/index.js
+++ app/angularApps/back/directives/index.js
@@ -3,3 +3,4 @@
 var app = require('angular').module('Back');
 app.directive('btnLogout', require('./logout.directive.js'));
 app.directive('formUploadImages', require('./uploadImages.directive.js'));
+app.directive('imgList', require('./imgList.directive.js'));
